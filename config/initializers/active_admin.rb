3e048698539e619663a308abc2fba9206a7b5c56
minahil3955 <minahil.mustafa@devsinc.com>
2021-04-12 13:00:28 +0500

    Customise Admina and Create New User Views

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,12 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-<<<<<<< HEAD
   config.site_title = 'Super Admin'
-=======
-  config.site_title = "Lead"
->>>>>>> Created Active_Admin and User Resources
-
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
