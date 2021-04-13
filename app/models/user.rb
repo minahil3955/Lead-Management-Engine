7fd3b22b0337a631fb9f3e68d0a314f0bb06bef6
minahil3955 <minahil.mustafa@devsinc.com>
2021-04-13 20:31:50 +0500

    Improved UI for User Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :registerable, :confirmable
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
   has_and_belongs_to_many :phases
   has_and_belongs_to_many :roles
   has_many :project_leads
