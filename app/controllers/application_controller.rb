564e324ea55af0b91e3c17f8aaaf32c380b12aca
minahil3955 <minahil.mustafa@devsinc.com>
2021-04-21 13:13:21 +0500

    User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @project_leads = ProjectLead.close
   end
